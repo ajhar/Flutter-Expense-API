14d3270b7dcb89d3b3ecc6997f1458eaf8558421
Mohamed Ajhar <mohamedajhar@gmail.com>
2022-04-18 15:56:53 +0530

    expense double to decimal

--- database/migrations/2022_04_12_054013_create_expenses_table.php
+++ database/migrations/2022_04_12_054013_create_expenses_table.php
@@ -17,7 +17,7 @@ class CreateExpensesTable extends Migration
             $table->id();
             $table->date('date')->index();
             $table->string('title');
-            $table->double('amount',8,2);
+            $table->decimal('amount',8,2);
             $table->timestamps();
         });
     }
